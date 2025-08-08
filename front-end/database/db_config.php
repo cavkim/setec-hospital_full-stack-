3e5d3ca43f2b45bf18d8533ff6c79446d34cecf0
Eath Mengsrong <athmengsrong2@gmail.com>
2025-08-08 16:34:15 +0700

    Update

--- front-end/database/db_config.php
+++ front-end/database/db_config.php
@@ -1,23 +1,17 @@
 <?php
-    $HOST = "localhost:3306";
+    $HOST = "localhost";
+    $PORT = 3306;
     $USER_NAME = "root";
-    $PASS = ""; 
+    $PASS = "CAVKiM@143";
 
-    $CONNECTION = mysqli_connect($HOST, $USER_NAME, $PASS);
+    // Pass port as 5th parameter, and database name here too
+    $CONNECTION = mysqli_connect($HOST, $USER_NAME, $PASS, "hospital_su30", $PORT);
 
     if (!$CONNECTION) {
-        print ("Your connection to database faild!").mysqli_connect_error();
+        die("Your connection to database failed! " . mysqli_connect_error());
     } 
 
-
-    // Select db
-
-    mysqli_select_db($CONNECTION, "hospital_su30")
-    or die ("Error connecting to database!");
-
-    // Support font khmer 
     mysqli_set_charset($CONNECTION, "utf8");
 
-
-    //  print("Your connection to database is successfully!");
+    // print("Your connection to database is successfully!");
 ?>
